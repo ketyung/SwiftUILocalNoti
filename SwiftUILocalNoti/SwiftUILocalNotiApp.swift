3326ff34fec2b87de8d8d8ef4fce9ce3b7abfdbd
techchee <ketyung@techchee.com>
2021-03-07 10:41:49 +0800

    Initial Commit

---
+++ SwiftUILocalNoti/SwiftUILocalNotiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILocalNotiApp.swift
+//  SwiftUILocalNoti
+//
+//  Created by Chee Ket Yung on 07/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILocalNotiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
